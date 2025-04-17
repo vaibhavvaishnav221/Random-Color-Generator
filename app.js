7aaa8911064d1c45514da8fddd1824aa5608a3a8
Vaibhav Vaishnav <vaibhavvaishnav221@gmail.com>
2025-04-17 07:22:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let heading = document.querySelector('h1');
+let div = document.querySelector('div');
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+
+    let red = Math.floor((Math.random() * 255) + 1);
+    let green = Math.floor((Math.random() * 255) + 1);
+    let blue = Math.floor((Math.random() * 255) + 1);
+
+    heading.style.color = `rgb(${red}, ${green}, ${blue})`
+    div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+});

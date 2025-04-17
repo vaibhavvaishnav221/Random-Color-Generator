df3336a3449a4d5491a10068d97b923544046a46
Vaibhav Vaishnav <vaibhavvaishnav221@gmail.com>
2025-04-17 07:39:05 +0530

    Update app.js
    Update the logic of color generation

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
 
-    let red = Math.floor((Math.random() * 255) + 1);
-    let green = Math.floor((Math.random() * 255) + 1);
-    let blue = Math.floor((Math.random() * 255) + 1);
+    let red = Math.floor((Math.random() * 255));
+    let green = Math.floor((Math.random() * 255));
+    let blue = Math.floor((Math.random() * 255));
 
     heading.style.color = `rgb(${red}, ${green}, ${blue})`;
     div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
